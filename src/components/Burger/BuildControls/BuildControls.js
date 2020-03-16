9c22c1b75a3a6fbe2a329d3508a792a1e8145b97
Michael Craig <mcraig@rpa.com>
2020-03-15 21:18:06 -0700

    Add ability to add ingredients to a burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,10 @@ const controls = [
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.labe} />
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)} />
         ))}
     </div>
 );
