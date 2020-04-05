3384bd2c345d72822efe784c49c2fbf9d0cdbd96
Michael Craig <mcraig@rpa.com>
2020-04-05 12:13:52 -0400

    Improve performance by limiting when components are updates.  Reorganized the project structure to include Auxiliary in the hoc folder structure.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    
-    const ingredientSummary = Object.keys(props.ingredients)
-                                .map(ingKey => {
-                                    return (
-                                        <li key={ingKey}>
-                                            <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
-                                        </li>
-                                    )
-                                })
-    
+class OrderSummary extends Component{
+    componentWillUpdate(){
+        console.log('[OrderSummar] Will Update')
+    }
 
-    return(
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button 
-                btnType="Danger"
-                clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button
-                btnType="Success"
-                clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
-};
+    render() {
 
-export default orderSummary;
+        const ingredientSummary = Object.keys(this.props.ingredients)
+                                    .map(ingKey => {
+                                        return (
+                                            <li key={ingKey}>
+                                                <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
+                                            </li>
+                                        )
+                                    })
+
+        return (    
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button 
+                    btnType="Danger"
+                    clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button
+                    btnType="Success"
+                    clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+}
+
+export default OrderSummary;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Auxiliary';
+import Aux from '../Auxiliary/Auxiliary';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
+
     state = {
         showSideDrawer: false
     }
@@ -12,9 +13,11 @@ class Layout extends Component {
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false})
     }
+
     sideDrawerOpenHandler = () => {
         this.setState({showSideDrawer: true})
     }
+    
     sideMenuToggleHandler = () => {
         this.setState((prevState) => {
             return { showSideDrawer: !prevState.showSideDrawer };
