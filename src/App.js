8db227a31851cbef20317374d056772bcb2640fc
Michael Craig <mcraig@rpa.com>
2020-04-11 15:16:17 -0400

    Optimize code to eject interceptors when they are no longer required.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  state = {
-    show: true
-  };
+class App extends Component {
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({show: false})
-    }, 5000);
+  render(){
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <Layout>
-        {this.state.show ? <BurgerBuilder /> : null}
-      </Layout>
-    </div>
-  );
 }
 
 export default App;
