8fb29b9901d838a8970df62d056e0068ec984c37
Michael Craig <mcraig@rpa.com>
2020-05-18 17:20:24 -0700

    Add form binding and generic form building capabilities

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -6,15 +6,39 @@ const input = (props) => {
     
     let inputElement = null;
 
-    switch (props.inputtype) {
+    switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.inputElement} {...props} />;
+            inputElement = <input 
+                            className={classes.inputElement} 
+                            {...props.elementConfig} 
+                            value={props.value}
+                            onChange={props.changed} />;
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.inputElement} {...props} />;
+            inputElement = <textarea 
+                            className={classes.inputElement} 
+                            {...props.elementConfig} 
+                            value={props.value}
+                            onChange={props.changed}   />;
+            break;
+        case ('select'):
+            inputElement = (
+                            <select
+                            className={classes.InputElement}
+                            value = {props.value}
+                            onChange={props.changed} >
+                                {props.elementConfig.options.map(option => (
+                                    <option key={option.value} value={option.value}>{option.displayValue}</option>
+                                ))}
+                            </select>
+            );
             break;
         default:
-            inputElement = <input className={classes.inputElement} {...props} />;
+            inputElement = <input 
+                            className={classes.inputElement} 
+                            {...props.elementConfig} 
+                            value={props.value}
+                            onChange={props.changed}   />;
     }
 
     return (
