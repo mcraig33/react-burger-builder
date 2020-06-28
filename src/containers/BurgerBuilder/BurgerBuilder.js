1cae7bd571f2450d9811fc173e2fc5e04f356934
Michael Craig <mcraig@rpa.com>
2020-06-28 14:40:55 -0400

    Add signup redirect to the order workflow if a user is not authenticated

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,7 +31,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseHandler = () => {
-        this.setState({purchasing: true});
+        if(this.props.isAuthenticated){
+        this.setState({purchasing: true})
+        }
+        else {
+            this.props.history.push('/auth');
+        };
     }
 
     purchaseCancelHandler = () => {
@@ -65,7 +70,8 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         price={this.props.price}
                         ordered={this.purchaseHandler}
-                        purchasable = {this.updatePurchaseState(this.props.ings)} />
+                        purchasable = {this.updatePurchaseState(this.props.ings)}
+                        isAuth={this.props.isAuthenticated} />
                 </Aux>
                 );
             orderSummary = <OrderSummary 
@@ -90,7 +96,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     };
 }
 
