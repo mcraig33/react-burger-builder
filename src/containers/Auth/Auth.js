4b21d54b3907f8ca3b65f9194434e8fa888db9a0
Michael Craig <mcraig@rpa.com>
2020-06-28 09:17:56 -0400

    Add timeout logout functionality

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 
@@ -85,6 +86,7 @@ class Auth extends Component {
 
     render () {
         const formElementsArray = [];
+
         for(let key in this.state.controls){
             formElementsArray.push({
                 id: key,
@@ -92,7 +94,7 @@ class Auth extends Component {
             });
         }
 
-        const form = formElementsArray.map(formElement => (
+        let form = formElementsArray.map(formElement => (
             <Input
                 key={formElement.id}
                 elementType={formElement.config.elementType} 
@@ -104,8 +106,21 @@ class Auth extends Component {
                     changed={(event) => this.inputChangedHandler(event,formElement.id)}
                     />
         ))
+
+        if(this.props.loading) {
+            form = <Spinner />
+        }
+
+        let errorMessage = null;
+        if(this.props.error){
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            )
+        }
+
         return(
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
@@ -124,4 +139,11 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+const mapStateToProps = state => {
+    return{
+        loading: state.auth.loading,
+        error: state.auth.error
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
