f60471be9f4814874b506763a302f03a0169888e
Michael Craig <mcraig@rpa.com>
2020-07-03 10:01:25 -0400

    Edit description of logout navigation test

--- src/components/Navigation/NavigationItems/navigationItems.test.js
+++ src/components/Navigation/NavigationItems/navigationItems.test.js
@@ -23,7 +23,7 @@ describe('<NavigationItems />',() => {
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
-    it('should render three <NavigationItems /> elements if authenticated', () => {
+    it('should render the logout navigation item if authenticated', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
