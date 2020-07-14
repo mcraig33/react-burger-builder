268bc19e84cf205a61d4df806824e9850c5eda5a
Michael Craig <mcraig@rpa.com>
2020-07-14 11:00:21 -0400

    414: Integrate Logut Saga

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import createSagaMiddleware from 'redux-saga';
 
 import './index.css';
 import App from './App';
@@ -11,6 +12,7 @@ import * as serviceWorker from './serviceWorker';
 import thunk from 'redux-thunk';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import {logoutSaga} from './store/sagas/auth'
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
@@ -20,9 +22,13 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
-const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
-    applyMiddleware(thunk)
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk, sagaMiddleware)
 ));
+
+sagaMiddleware.run(logoutSaga);
 
 const app = (
     <Provider store={store}>
